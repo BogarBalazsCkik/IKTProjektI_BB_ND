ae237cb09cf25b24352c2a46eed339f3ae053251
Balazs <futurebalazs@gmail.com>
2024-12-19 11:20:20 +0100

    a feladat BB

--- IKTProjektI_8feladat_BB_ND.py
+++ IKTProjektI_8feladat_BB_ND.py
@@ -6,7 +6,7 @@ lista=[2, 1, 2, 3, 5, 12, 6, 6, 4, 3]
 print("a) feladat:")
 tA=[2, 1, 2, 3, 5, 7, 6, 6, 4, 3]
 otneltobb=0
-for i in tA:
+for i in (tA):
     if(i<5):
         otneltobb+=1
 print(otneltobb,"napon mértünk maximum 5 fokot")
