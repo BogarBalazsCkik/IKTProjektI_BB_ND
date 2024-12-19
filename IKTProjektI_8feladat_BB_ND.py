0eb193e265d9b1c0ea5158ffb8cb1d4ff1590041
Dominik <dnemeth181@gmail.com>
2024-12-19 11:24:26 +0100

    b feladat ND

--- IKTProjektI_8feladat_BB_ND.py
+++ IKTProjektI_8feladat_BB_ND.py
@@ -10,3 +10,12 @@ for i in (tA):
     if(i<5):
         otneltobb+=1
 print(otneltobb,"napon mértünk maximum 5 fokot")
+
+print("b) feladat:")
+tB=[2, 1, 2, 3, 5, 7, 6, 6, 4, 3]
+kulonbseg=0
+poz1=0
+poz2=0
+for i in range(len(lista)-1):
+    if((lista[i+1]-lista[i]>=kulonbseg)):
+        
